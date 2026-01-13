516d6be7eeddb539356fe24d6a6b9221f6cf27e2
Prilepskiy AE <alexey.prilepskiy1992@gmail.com>
2026-01-14 02:04:55 +0700

    add logic dead

--- Assets/Scripts/PlayerControlle.cs
+++ Assets/Scripts/PlayerControlle.cs
@@ -1,17 +1,17 @@
 
 using UnityEngine;
 
-public enum PlayerState { Go, AttackEnemy, Dead }
+public enum PlayerState { Go,RunAttack, AttackEnemy, Dead }
 
 public class PlayerControlle : MonoBehaviour 
 {
     [Header("Movement Settings")]
     [SerializeField] private float _walkSpeed = 3f;
     
-    [Header("References")]
-    [SerializeField] private Animator _animator;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
-    [SerializeField] private float _runSpeed = 6f;
+    
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private float _runSpeed = 6f;
     
     [Range(0f, 1f)]
     [SerializeField] private float _acceleration = 0.4f;
@@ -25,7 +25,6 @@ public class PlayerControlle : MonoBehaviour
 
     private void Awake()
     {
-        // Оптимизировано: проверка и получение компонентов
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _infoPlayer = GetComponent<InfoPlayer>();
@@ -40,6 +39,7 @@ public class PlayerControlle : MonoBehaviour
             case PlayerState.Go: Go(); break;
             case PlayerState.AttackEnemy: Attack(); break;
             case PlayerState.Dead: Dead(); break;
+            case PlayerState.RunAttack: RunAttack();break;
 
         }
     }
@@ -61,17 +61,15 @@ public class PlayerControlle : MonoBehaviour
 
     private void Attack()
     {
-        // Исправлено: логика атаки
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _animator.SetBool("Attack", _isMoving && _isRunning);
-            if (!_isRunning)
-            {
-                _animator.SetBool("SAttack1", true);
-            }
-        }
+    _animator.SetBool("SAttack1", true);     
+    _currentState = PlayerState.Go;
+    }
 
-        _currentState = PlayerState.Go;
+    private void RunAttack()
+    {
+     Debug.Log("EVENT");  
+    _animator.SetBool("Attack", true);     
+    _currentState = PlayerState.Go;
     }
 
     private void HandleInput()
@@ -93,15 +91,25 @@ public class PlayerControlle : MonoBehaviour
     {
         float movementThreshold = 0.1f;
         _isMoving = Mathf.Abs(_horizontalInput) > movementThreshold;
-        
+        _animator.SetBool("SAttack1", false);
+    
         _animator.SetBool("Walk", _isMoving);
         _animator.SetBool("Run", _isMoving && _isRunning);
-
+ 
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isRunning)
+        {
+            _currentState = PlayerState.RunAttack;
+        }else
+        {
+            _animator.SetBool("Attack", false);  
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !_isRunning)
         {
             _currentState = PlayerState.AttackEnemy;
         }
+        
     }
 
     private void Dead()
