b5ddb6a6475b85df1623e44027359caa4510d76b
Prilepskiy AE <alexey.prilepskiy1992@gmail.com>
2026-01-24 00:01:41 +0700

    Player Attack

--- Assets/Scripts/EnamyNavigation.cs
+++ Assets/Scripts/EnamyNavigation.cs
@@ -2,14 +2,14 @@ using Unity.VisualScripting;
 using UnityEngine;
 public interface IShase
 {
-    void  IsShase(bool action);
+    void IsShase(bool action);
 }
-public class EnamyNavigation : MonoBehaviour,IShase
+public class EnamyNavigation : MonoBehaviour, IShase
 {
-    public enum EnemyState { Go,AttackPlayer, Dead, cShase }
+    public enum EnemyState { Go, AttackPlayer, cShase }
     public EnemyState currentState;
     public Transform[] points;
-    private float speed=1;
+    private float speed = 1;
     private Transform targetPoint;
     private int currentPoint;
     public bool cyclr = true;
@@ -21,109 +21,115 @@ public class EnamyNavigation : MonoBehaviour,IShase
 
     private float _threshold = 1.1f;
 
-    private bool detected=true;
+    private bool detected = true;
+
+    private bool isLive = true;
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
-        sprite=GetComponentInChildren<SpriteRenderer>();
-        currentState=EnemyState.Go;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        currentState = EnemyState.Go;
         forward = false;
-        currentPoint = 0;    
+        currentPoint = 0;
         targetPoint = points[currentPoint];
-        infoEnany= GetComponent<InfoEnany>();
+        infoEnany = GetComponent<InfoEnany>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        switch (currentState)
+        if (infoEnany.isLive)
         {
-            case EnemyState.Go: Go(); break;
-            case EnemyState.AttackPlayer: AttackPlayer(); break;
-            case EnemyState.Dead: Dead(); break;
-            case EnemyState.cShase: Shase(); break;
+            switch (currentState)
+            {
+                case EnemyState.Go: Go(); break;
+                case EnemyState.AttackPlayer: AttackPlayer(); break;
+                case EnemyState.cShase: Shase(); break;
+            }
         }
+        else
+        {
+            Dead();
+        }
+
     }
-    void Dead(){
-         animator.SetBool("dead", true);
-    }
-void Shase()
-{
-         animator.SetBool("attak", false);
-   GameObject player = GameObject.FindGameObjectWithTag("Player");
-    
-    if (player == null)
+    void Dead()
     {
-        Debug.LogWarning("Игрок не найден!");
-        return;
-    }
+        if (isLive)
+        {
+            isLive = false;
+             animator.SetBool("attak", false);
+            animator.SetTrigger("Dead");
+        }
 
-    if (points == null || points.Length < 2)
+    }
+    void Shase()
     {
-        Debug.LogError("Массив points не содержит достаточно точек!");
-        return;
+        animator.SetBool("attak", false);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null)
+        {
+            Debug.LogWarning("Игрок не найден!");
+            return;
+        }
+
+        if (points == null || points.Length < 2)
+        {
+            Debug.LogError("Массив points не содержит достаточно точек!");
+            return;
+        }
+
+        Vector2 currentPos = transform.position;
+        float targetX = player.transform.position.x;
+
+        float minX = Mathf.Min(points[0].position.x, points[1].position.x);
+        float maxX = Mathf.Max(points[0].position.x, points[1].position.x);
+
+
+
+
+        targetX = Mathf.Clamp(targetX, minX, maxX);
+
+        float newX = Mathf.MoveTowards(
+            currentPos.x,
+            targetX,
+            speed * Time.deltaTime
+        );
+        newX = Mathf.Clamp(newX, minX, maxX);
+        bool reachedLeft = Mathf.Approximately(newX, minX);
+        bool reachedRight = Mathf.Approximately(newX, maxX);
+
+        if (reachedLeft || reachedRight) animator.SetTrigger("IdleOk");
+        transform.position = new Vector2(newX, currentPos.y);
     }
-
-    Vector2 currentPos = transform.position;
-    float targetX = player.transform.position.x;
-
-    float minX = Mathf.Min(points[0].position.x, points[1].position.x);
-    float maxX = Mathf.Max(points[0].position.x, points[1].position.x);
-
-
-
-  
-    targetX = Mathf.Clamp(targetX, minX, maxX);
-
-    float newX = Mathf.MoveTowards(
-        currentPos.x,
-        targetX,
-        speed * Time.deltaTime
-    );
-    newX = Mathf.Clamp(newX, minX, maxX);
-    bool reachedLeft  = Mathf.Approximately(newX, minX);
-    bool reachedRight = Mathf.Approximately(newX, maxX);
-
-
-    if (reachedLeft || reachedRight) animator.SetTrigger("IdleOk");
-        
-        
-        
-
-    transform.position = new Vector2(newX, currentPos.y);
-}
 
     void Go()
     {
-        if (!infoEnany.isLive)
-        {
-            currentState = EnemyState.Dead;
-        }
-         animator.SetBool("attak", false);
-         animator.SetTrigger("IdleNok");
+        animator.SetBool("attak", false);
+        animator.SetTrigger("IdleNok");
 
-    if (Vector3.Distance(transform.position, targetPoint.position) < _threshold)
+        if (Vector3.Distance(transform.position, targetPoint.position) < _threshold)
         {
-            
+
             if (forward) currentPoint++; else currentPoint--;
             if (currentPoint >= points.Length && cyclr)
             {
                 currentPoint = 0;
-               
+
             }
             else if (currentPoint >= points.Length && !cyclr)
             {
                 currentPoint = points.Length - 2;
                 forward = false;
-                
+
             }
             else if (currentPoint < 0)
             {
                 forward = true;
                 currentPoint = 1;
-                
+
             }
             sprite.flipX = currentPoint == 0;
             targetPoint = points[currentPoint];
@@ -131,28 +137,25 @@ void Shase()
         transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
     }
 
-    void AttackPlayer() {
-        if (!infoEnany.isLive)
-        {
-            currentState = EnemyState.Dead;
-        }
-         animator.SetTrigger("IdleNok");
-         animator.SetBool("run", false);
-         animator.SetBool("attak",true);
+    void AttackPlayer()
+    {
+        animator.SetTrigger("IdleNok");
+        animator.SetBool("run", false);
+        animator.SetBool("attak", infoEnany.isLive);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && infoEnany.isLive )
         {
-            
-            currentState=EnemyState.AttackPlayer;
+
+            currentState = EnemyState.AttackPlayer;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && infoEnany.isLive)
         {
             currentState = EnemyState.cShase;
         }
@@ -160,18 +163,20 @@ void Shase()
 
     public void IsShase(bool action)
     {
-        
+
         if (action)
         {
             if (detected)
             {
-               currentState = EnemyState.cShase; 
-               detected=false;
+                currentState = EnemyState.cShase;
+                detected = false;
             }
-            
-        } else {
+
+        }
+        else
+        {
             currentState = EnemyState.Go;
-            detected=true;
-            }
+            detected = true;
+        }
     }
 }
--- Assets/Scripts/InfoEnany.cs
+++ Assets/Scripts/InfoEnany.cs
@@ -2,25 +2,15 @@ using UnityEngine;
 
 public class InfoEnany : MonoBehaviour
 {
-    
+
     public float HP = 100f;
     public float maxHP = 100f;
-    public bool isLive=true;
+    public bool isLive = true;
 
-    public void Damage(float damage)
-    {
-        HP -= damage;
-        Debug.Log(HP);
-        Debug.Log("=========");
-    }
+    public void Damage(float damage) => HP -= damage;
 
-    public void UpHP(float value)
-    {
-        HP += value;
-        Debug.Log(HP);
-    }
+    public void UpHP(float value) => HP += value;
 
-  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,7 +20,7 @@ public class InfoEnany : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (HP<=0)
+        if (HP <= 0)
         {
             isLive = false;
 
