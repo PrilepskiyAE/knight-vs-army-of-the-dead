a462005f5190ab370d9dc5320f64856a5eb92bd1
Prilepskiy AE <alexey.prilepskiy1992@gmail.com>
2026-01-24 22:10:11 +0700

    add stanina

---
+++ Assets/Scripts/EndureBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndureBar : MonoBehaviour
+{
+ public Image endureBar;
+    public InfoPlayer player;
+
+    [System.Obsolete]
+    void Start()
+    {
+        endureBar = GetComponent<Image>();
+        player =  FindObjectOfType<InfoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        endureBar.fillAmount = player.ST / player.maxSt;
+    }
+}
--- Assets/Scripts/InfoPlayer.cs
+++ Assets/Scripts/InfoPlayer.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InfoPlayer : MonoBehaviour
@@ -6,13 +7,40 @@ public class InfoPlayer : MonoBehaviour
     public float HP = 100f;
     public float maxHP = 100f;
 
+    public float ST = 100f;
+    public float maxSt = 100f;
+
+    private float timer=0;
+
+    public float recoveryDelay = 1f;     
+
     public void Damage(float damage)
     {
         HP -= damage;
+        ST -= 5; 
     }
 
     public void UpHP(float value) {
     HP += value;
+    }
+
+    void Update()
+    {
+
+        if (ST < maxSt)
+        {
+            timer += Time.deltaTime;
+            if (timer >= recoveryDelay)
+            {
+                ST++;
+                timer -= recoveryDelay;  // Обнуляем таймер (с учётом «перелива»)
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+
     }
 
    
